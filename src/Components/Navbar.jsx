5bcf54ff10838ee1aa35f1e18f27b2da70a64942
gastonruiz <gastonruizojeda@gmail.com>
2025-03-02 11:47:21 -0300

    navbar y logo insta

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,26 +18,42 @@ link: '/'
         },
 ]
 
+const navbarRedes = [
 
+{
+        id: 1,
+        title: 'Instagram',
+        link: 'aca va la pagina de instagram a linkear',
+        icon:'/icons/instagram.png',
+        },
+      
+]
 
 const Navbar = () => {
   return (
     <nav>
-        <div>
+        <div className='flex justify-between items-center sm:px-12 sm:py-6 px-4 py-3 '>
              <div>
-                <img src={LogoNav} alt="inside spanish" className='w-100'/>
+                <img src={LogoNav} alt="inside spanish" className='w-55'/>
             </div>
             <div>
-                <ul>
-             <li></li>
+            <ul className='flex sm:space-x-8  space-x-4'>
+                   {navbarLinks.map((link)=>(
+                <li key={link.id}>
+                    <a className='text-gray-700 text-sm' 
+                    href={link.link}>{link.title}</a>
+                </li>
+                   ))}
                 </ul>
             </div>
 
             <div>
                 <ul>
-                   {navbarLinks.map((link)=>(
-                <li key={link.id}>
-                    <a>{link.title}</a>
+                   {navbarRedes.map((red)=>(
+                <li key={red.id}>
+                    <a href={red.link}>
+                    <img src={red.icon} alt={red.title} className="w-6 h-6" />
+                    </a>
                 </li>
                    ))}
                 </ul>
